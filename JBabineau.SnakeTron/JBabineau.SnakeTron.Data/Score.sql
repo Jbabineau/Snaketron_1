ad3ede92e706d9637ac5d10bcd5384b0deaaf504
Jbabineau <babineau.james@gmail.com>
2016-07-21 00:19:15 -0400

    Starting the database to hold the leaderboard

---
+++ JBabineau.SnakeTron/JBabineau.SnakeTron.Data/Score.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Score]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[UserName] VARCHAR(40) NOT NULL,
+	[Score] INT NOT NULL,
+	[Kills] INT NOT NULL,
+	[Blocks] INT NOT NULL,
+	[Missed] INT NOT NULL,
+	[DateSubmitted] DATETIME NOT NULL
+
+)
---
+++ JBabineau.SnakeTron/JBabineau.SnakeTron/Controllers/ScoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JBabineau.SnakeTron.Controllers
+{
+    public class ScoreController : ApiController
+    {
+        // Get: 
+        public List<Score> GetScores()
+        {
+            List<Score> result = new List<Score>();
+
+            using (SnaketronEntities ctx = new SnaketronEntities())
+            {
+                result = ctx.Scores.Take(10).OrderBy(s => s.Score1).ToList();
+            }
+
+            return result;
+        }
+
+        public void AddScore()
+        {
+
+        }
+    }
+}
